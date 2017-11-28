3b98f0c50e42097f548c100c486e87c80384b207
sultana25 <ssazeda@gmail.com>
2017-11-28 01:10:49 -0800

    code header and navigation section

---
+++ index.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        
+        <meta http-equiv="content-type" content="text/html;
+        charset=UTF-8">
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+        <link rel="icon" href="assets/img/favicon.jpg">
+        <title>Bootstrap to Wordpress</title>
+        <!-- Bootstrap core CSS -->
+        
+        <link href="assets/css/bootstrap.min.css"  rel="stylesheet">
+        <link class="assets/css/style.css" rel="stylesheet">
+        <!-- FontAwosome Icon-->
+        <link href="assets/css/font-awesome/css/font-awesome.css" rel="stylesheet">
+        <!-- Google Font -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet"> 
+        <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!--[if lt IE 9]>
+          <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+          <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <!-- HEADER SECTION -->
+        <header class="site-header" role="banner">
+          <!--Navbar-->
+           <div class="navbar-wrapper">
+              <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+                 <div class="container">
+                     <div class="navbar-header">
+                         <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                             <span class="sr-only">Toggle navigation</span>
+                             <span class="icon-bar"></span>
+                             <span class="icon-bar"></span>
+                             <span class="icon-bar"></span>
+                             
+                         </button>
+                         <a class="navbar-brand" href="/">BootstrapToWordpress</a>
+                        </div><!-- navbar-header-->
+                        <div class="navbar-collapse collapse">
+                        
+                         <ul class="nav navbar-nav navbar-right">
+                            <li class="active"><a href="/">Home</a></li>
+                            <li><a href="blog.html">Blog</a></li>
+                            <li><a href="resource.html">Resource</a></li>
+                            <li><a href="contact.html">Contact</a></li>\
+                            ]
+                         </ul>
+                     </div>
+                     </div><!--container-->
+                     
+                     
+                 </div><!--navbar-->
+                  
+              
+               
+           </div><!-- navbar wrapper-->
+            
+        </header>
+        <!-- Hero-->
+        <section id="hero">
+           <article>
+               <div class="container clearfix">
+                  <div class="row">
+                     <div class="col-sm-5">
+                        <img src="assets/img/logo.jpg" alt="Bootstrap to Wordpress" class="logo">
+                     </div><!--col-->
+                     <div class="col-sm-7 hero-text">
+                         <h1>Bootstrap to Wordpress</h1>
+                         <p class="lead">f that address is correct, here are three other things you can tryf that address is correct, here are three other things you can tryf that address is correct, here are three other things you can try</p>
+                         <div class="price-timeline">
+                             <div class="price active">
+                                 <h4 class="price">pre-launch price<small>End soon!!</small></h4>
+                                 <span>$399</span>
+                             </div>
+                             <div class="price">
+                                 <h4 class="price">launch price<small>Coming soon!!</small></h4>
+                                 <span>$399</span>
+                             </div>
+                             <div class="price active">
+                                 <h4 class="price">Final-launch price<small>Coming soon!!</small></h4>
+                                 <span>$399</span>
+                             </div>
+                         </div><!--price timeline-->
+                         <p><a href="/" class="btn btn-lg btn-danger" role="button">Enroll &raquo;</a></p>
+                     </div><!--col hero-text-->
+                      
+                  </div><!--row-->
+                   
+               </div><!--container-->
+           </article>
+            
+        </section><!--hero-->
+        <!-- Opt in section-->
+        <section></section>
+        <!-- Boost your income-->
+        <section></section>
+        <!-- Who benefits-->
+        <section></section>
+        <!-- Course features-->
+        <section></section>
+        <!-- Project features-->
+        <section></section>
+        <!-- Video Features-->
+        <section></section>
+        <!-- instructor-->
+        <section></section>
+        <!-- Testimonials-->
+        <section></section>
+        <!-- footer-->
+        <footer></footer>
+        <!-- Modals-->
+        <div></div>
+        <!-- Bootstrap core javascript
+        placed at the end of the document so pages can load fast-->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script src="assets/main.js"></script>
+        <!-- Typekit Fonts -->
+        <script src="//use.typekit.net/gla7wnd.js"></script>
+        <script>try{Typekit.load();}catch(e){}</script>
+    </body>
+</html>
